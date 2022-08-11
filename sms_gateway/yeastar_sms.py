ad01dabc6798990245cd1785afb6b4018aa09ad5
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-11 02:41:27 +0200

    [wip] Writing script to send SMS to users via Yeastar gateway

---
+++ sms_gateway/yeastar_sms.py
@@ -0,0 +1,28 @@
+import sqlite3
+from datetime import datetime, timedelta
+
+connect = sqlite3.connect('../db.sqlite3')
+
+c = connect.cursor()
+
+danger_alert = datetime.today()
+warning_alert = datetime.today() + timedelta(days=30)
+
+
+for row in c.execute('SELECT car_number, car_service_inspection_date, car_technical_inspection_date FROM cars_car'):
+    car_service_inspection_date = datetime.strptime(row[1], '%Y-%m-%d')
+    car_technical_inspection_date = datetime.strptime(row[2], '%Y-%m-%d')
+
+    if danger_alert > car_service_inspection_date:
+        print(f'Danger: {car_service_inspection_date}')
+    elif warning_alert > car_service_inspection_date:
+        print(f'Warning: {car_service_inspection_date}')
+    elif danger_alert < car_service_inspection_date:
+        print(f'OK: {car_service_inspection_date}')
+
+    if danger_alert > car_technical_inspection_date:
+        print(f'Danger: {car_technical_inspection_date}')
+    elif warning_alert > car_technical_inspection_date:
+        print(f'Warning: {car_technical_inspection_date}')
+    elif danger_alert < car_technical_inspection_date:
+        print(f'OK: {car_technical_inspection_date}')
