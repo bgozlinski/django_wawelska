3b253d73ea77582c3c8ac5f229f20500f157daa1
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-17 09:47:10 +0200

    hidding secret key

--- wawelska/settings.py
+++ wawelska/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(c^398r5h9dg(^%-)b@$c9t1b#x#u!!j3d3ojz(j(0f2lq#!4l'
+# SECRET_KEY = 'django-insecure-(c^398r5h9dg(^%-)b@$c9t1b#x#u!!j3d3ojz(j(0f2lq#!4l'
+SECRET_KEY = config("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
