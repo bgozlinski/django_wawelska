86378de477ae517190f277311cf1ce7ad3625d0f
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-19 10:07:20 +0200

    yeastar minor changes

--- sms_gateway/yeastar_gateway.py
+++ sms_gateway/yeastar_gateway.py
@@ -28,7 +28,7 @@ class Yeastar:
         data = self.s.recv(self.BUFFER_SIZE)
         print(data)
         if data.decode().__contains__("Asterisk"):
-            data = self.send_command(message=f"Action: login\r\nUsername: {self.username}\r\nSecret: {self.secret}\r\n\r\n".encode(),
+            data = self.send_command(message=f"Action: login\nUsername: {self.username}\nSecret: {self.secret}\n\n".encode(),
                                      timeout=3
                                      )
 
@@ -53,7 +53,7 @@ class Yeastar:
 
     def send_sms(self, sim_port: int, phone_number: str, message: str):
         self.send_command(
-            message=f'Action: smscommand\r\ncommand: gsm send sms {sim_port+1} {phone_number} "{message}" {generate_id}\r\n\r\n'.encode(),
+            message=f'Action: smscommand\ncommand: gsm send sms {sim_port+1} {phone_number} "{message}" {generate_id}\n\n'.encode(),
             timeout=6
         )
         print(f'Sending sms to {phone_number} ...')
