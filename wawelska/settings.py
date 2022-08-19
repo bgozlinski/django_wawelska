38ab676a364f6e1d846177da6b4579b7f7239c71
bgozlinski <108179667+bgozlinski@users.noreply.github.com>
2022-08-20 01:15:34 +0200

    Update settings.py

--- wawelska/settings.py
+++ wawelska/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-(c^398r5h9dg(^%-)b@$c9t1b#x#u!!j3d3ojz(j(0f2lq#!4l'
 SECRET_KEY = config("SECRET_KEY")
 
 
